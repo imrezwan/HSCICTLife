1d5148c85d9a48d30e3aeea3274e2491047749d0
RKPranto <rezwan2525@gmail.com>
2020-09-09 13:18:35 +0600

    splash screen

---
+++ app/src/main/java/com/rezwan_cs/hscictlife/activities/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.rezwan_cs.hscictlife.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rezwan_cs.hscictlife.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
